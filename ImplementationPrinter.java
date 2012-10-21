d53a52f223ffe9aef0864ea672ea956d9acdb425
Vivek Patel <Vvick.727@gmail.com>
2012-10-21 17:30:16 -0400

    Worked more on ImplementationWriter. not working

--- ImplementationPrinter.java
+++ ImplementationPrinter.java
@@ -30,6 +30,11 @@ public class ImplementationPrinter extends Visitor {
 	protected final Printer printer;
 
 	/**
+	 * The current class in the traversal.
+	 */
+	protected String currentClass;
+
+	/**
 	 * Create a new C++ printer.
 	 *
 	 * @param printer The printer.
@@ -53,11 +58,17 @@ public class ImplementationPrinter extends Visitor {
 	}
 
 
+	public void visitCompilationUnit(GNode n) {
+		visit(n);
+		printer.pln("int main() {}");
+	}
+
 	public void visitPragmaDirective(GNode n) {
 		printer.p("#pragma ").pln(n.getString(0));
 	}
 
 	public void visitIncludeDirectives(GNode n) {
+		visit(n);
 		printer.pln();
 	}
 
@@ -73,6 +84,7 @@ public class ImplementationPrinter extends Visitor {
 
 	}
 
+
 	public void visitUsing(GNode n) {
 
 	}
@@ -82,7 +94,71 @@ public class ImplementationPrinter extends Visitor {
 	}
 
 	public void visitClassDeclaration(GNode n) {
+		this.currentClass = n.getString(CLASS_NAME);
 
+		// vtable
+		printer.p("__")
+		// FIXME
+
+		// class constructor
+		printer.p("__").p(n.getString(CLASS_NAME).p("::__")
+				.p(n.getString(CLASS_NAME)
+				.pln("() : __vptr(&__vtable) {");
+		visit(n);
+		printer.pln("}");
+	}
+
+	public void visitImplementedMethods(GNode n) {
+		visit(n);
+	}
+
+	/** Only visited in implemented methods */
+	public void visitMethodDeclaration(GNode n) {
+		visit(n.getGeneric(0)); // return type
+		printer.p(" __").p(currentClass).p("::");
+		printer.p("::").p(n.getString(1)); // method name
+		visit(n.getGeneric(2)); // parameters
+		printer.p(" {");
+		visit(n.getGeneric(3)); // block
+	}
+
+	public void visitFrom(GNode n) {
+		printer.p(n.getString(0));	
+	}
+
+	public void visitExpression(GNode n) {
+		visit(n.getGeneric(0));
+		printer.p(' ').p(n.getString(1)).p(' ');
+		visit(n.getGeneric(3));
+		printer.pln(";");
+	}
+
+	public void visitPrimaryIdentifier(GNode n) {
+		printer.p(n.getString(0));
+	}
+
+	public void visitStringLiteral(GNode n) {
+		printer.p("__rt::literal(").p(n.getString(0))p(')');
+	}
+
+	public void visitFormalParameters(GNode n) {
+		printer.p('(').p(currentClass).p(" __this");
+		for (Iterator<?> iter = n.iterator(); iter.hasNext(); ) }
+			if (iter.hasNext()) {
+				printer.p(", ");
+			}
+			printer.p((Node)iter.next());
+		}
+		printer.p(')');
+	}
+
+	public void visitType(GNode n) {
+		printer.p(n.getString(0));
+	}
+
+	public void visitFormalParameter(GNode n) {
+		visit(n.getGeneric(0));
+		printer.p(' ').p(n.getString(1));
 	}
 
 	public void visitTypedefSpecifier(GNode n) {
@@ -100,8 +176,13 @@ public class ImplementationPrinter extends Visitor {
 	public void visitReturnStatement(GNode n) {
 		printer.p("return");
 		if (null != n.getNode(0)) {
-			printer.p(' ').p(n.getNode(0));
+			printer.p(' ');
+			visit(n.getNode(0));
 		}
 		printer.pln(';');
+	}
+
+	public void visit(Node n) {
+		for (Object o : n) if (o instanceof Node) dispatch((Node)o);
 	}
 }
