459d54cacdfdf85054a44a171a6ca34ffc34255f
Vivek Patel <Vvick.727@gmail.com>
2012-11-13 12:35:40 -0500

    Started Qimpp

---
+++ Qimpp.java
@@ -0,0 +1,21 @@
+
+
+import xtc.tree.Visitor;
+
+
+/**
+ * A Java to C++ translator.
+ *
+ * @author QIMPP
+ */
+class Qimpp {
+
+  /**
+   * Run the translator.
+   *
+   * @param args A Java file to translate.
+   */
+  public static void main(String[] args) {
+    new Qimpp().run(args);
+  }
+}
