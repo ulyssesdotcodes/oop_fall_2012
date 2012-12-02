109c9e5101d89f28162da5077c942e137125d483
Emily McAllister <emily@debian.24.29.103.16>
2012-12-02 16:19:54 -0500

    TestScopes compiles

--- tests/TestScopes.java
+++ tests/TestScopes.java
@@ -2,9 +2,8 @@ package qimpp.tests;
 
 public class TestScopes {
   static int foo = 3;
-  public void Bar{
+  public void Bar(){
     String bar = "hello";
-    System.out.println(bar);
   }
   public static void main ( String[] args ) {
     System.out.println(foo);
