41614a8bfc08f96ca14e8c3c0c04093d2c850da4
Alexandra Qin <alexandraqin@gmail.com>
2012-12-15 13:39:46 -0500

    added testNotEquals. 9/22 OK 5/22 fail diff

---
+++ tests/TestNotEquals.java
@@ -0,0 +1,15 @@
+package qimpp.tests;
+
+/** Test basic number types and arithmetic */
+public class TestNotEquals {
+  
+  public static void main ( String[] args ) {
+  
+    int x = 10;
+    if (x != 1) {
+      System.out.println("not equal");
+    }    
+
+  }
+
+}
