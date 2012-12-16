4e6e221a63ed9b0612f7ec32d934239226ab36ba
Alexandra Qin <alexandraqin@gmail.com>
2012-12-16 14:21:05 -0500

    forgot to add new test to last commit

---
+++ tests/Child2.java
@@ -0,0 +1,20 @@
+package qimpp.tests;
+
+/** Test basic number types and arithmetic */
+public class Child2 extends TestMethodCall {
+
+  public Child2() {
+
+  }
+
+  /*
+  public Object m1() {
+    return TestMethodCall.R2;
+  }
+
+  // -------------------------------------------------------------------------
+
+  public Object m2() {
+    return TestMethodCall.R4;
+  } */
+}
